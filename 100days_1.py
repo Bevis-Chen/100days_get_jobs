59c14ce8911c80bf4cd7d1c8d557ea8a145db5f0
austin.chen <austin.chen@gigacomputing.com>
2024-12-22 19:12:38 +0800

    只是完成day2-練習2

--- 100days_1.py
+++ 100days_1.py
@@ -3,8 +3,17 @@
 # JobS!!!
 
 # 練習1：華氏溫度轉換為攝氏溫度。
-_F = eval(input("輸入華氏溫度 : "))
-_C = (_F - 32) / 1.8
-print("華氏溫度: ", _F,  "\n攝氏溫度: ", _C)
+# _F = eval(input("輸入華氏溫度 : "))
+# _C = (_F - 32) / 1.8
+# print("華氏溫度: ", _F,  "\n攝氏溫度: ", _C)
 
-
+# 練習2：輸入圓的半徑計算計算周長和麵積。
+import math 
+r = eval(input("輸入圓的半徑: "))
+perimeter = 2 * r * 3.14159
+area = r * r *3.14159
+print(
+    "周長是: ", perimeter, 
+    "\n", 
+    "面積是: ", area
+)
